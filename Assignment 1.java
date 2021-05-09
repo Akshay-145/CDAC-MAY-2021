bb0936bd98515c9ed4ce047cb9744f1a358d5278
Akshay-145 <83825170+Akshay-145@users.noreply.github.com>
2021-05-09 10:54:16 +0530

    Add files via upload

---
+++ Assignment 1.java
@@ -0,0 +1,328 @@
+/*
+class Assg1
+{
+public static void main(String args[])
+{
+System.out.print("Hello");
+}
+}*/
+/*
+class Assg1
+{
+public static void main(String args[])
+{
+	int a=74;
+	int b=36;
+	int c=a+b;
+System.out.print("Addition : "+c);
+}
+}*/
+/*
+class Assg1
+{
+public static void main(String args[])
+{
+
+int c=50/3;
+System.out.print("Division : "+c);
+}
+}*/
+/*
+class Assg1
+{
+public static void main(String args[])
+{
+	int a=-5+(8*6);
+	int b=(55+9)%9;
+	int c=20+(-3*5)/8;
+	int d=5+15/(3*2)-(8%3);
+System.out.println(a);
+System.out.println(b);
+System.out.println(c);
+System.out.println(d);
+}
+}*/
+/*
+class Assg1
+{
+public static void main(String args[])
+{
+	int a=25;
+	int b=5;
+	int c=a*b;
+	
+System.out.print("multiplication :"+c);
+}
+}*/
+
+/*
+class Assg1
+{
+public static void main(String args[])
+{
+	int a=125;
+	int b=24;
+	int c=a+b;
+	int d=a-b;
+	int e=a*b;
+System.out.println("Add :"+c);
+System.out.println("Substract :"+d);
+System.out.println("multiplication :"+e);
+}
+}*/
+/*
+class Assg1
+{
+public static void main(String args[])
+{
+System.out.print("Table of 8....");
+for(int i=1;i<=10;i++)
+{
+	int mul=8*i;
+	System.out.println(mul);
+}
+}
+
+}*/
+
+/*
+class Assg1
+{
+public static void main(String args[])
+{
+for(int i=1;i<=5;i++)                                         
+                                                        														
+{
+	for(int j=5;j>=1;j--)
+	{
+		if(j<=i)
+		{
+			System.out.print(" * ");
+		}
+		else
+		{
+			System.out.print(" ");
+		}
+	}
+	System.out.println();
+
+}
+}
+}*/
+
+/*
+class Assg1
+{
+	public static void main(String args[])
+	{
+		double a=(25.5*3.5-3.5*3.5)/(40.5-4.5);
+		
+		System.out.print(a);
+	}
+}
+*/
+/*
+class Assg1
+{
+	public static void main(String args[])
+	{
+		double raduis=7.50000;
+		double perimeter=3.14*raduis*raduis;
+		double area=2*3.1456*raduis;
+		
+		System.out.println("Area :"+area);
+		System.out.println("perimeter :"+perimeter);
+		
+	}
+}
+*/
+/*
+class Assg1
+{
+	public static void main(String args[])
+	{
+		double width=5.5;
+		double hight=8.5;
+		double area=width*hight;
+		double perimeter=2*(width+hight);
+		
+		System.out.println("Area :"+area);
+		System.out.println("perimeter :"+perimeter);
+		
+	}
+}*/
+
+/*
+class Assg1
+
+{
+	public static void main(String args[])
+	{
+		for(int i=0;i<=10;i++)
+		{
+			for(int j=0;j<=20;j++)
+			{
+				if(j<=5 && i<=5)
+				{
+					System.out.print("*");
+				}
+				else
+				{
+					System.out.print("=");
+				}
+			}
+			System.out.println();
+}}}
+*/
+/*
+class Assg1
+
+{
+	public static void main(String args[])
+	{
+		System.out.println("+''''''+");
+	    System.out.println("[| o o |]");
+		System.out.println(" |  ^  |");
+		System.out.println(" | '_' |");
+		System.out.println("+''''''+");
+		
+}}*/
+
+
+
+/*class Assg12
+{
+public static void main(String args[])
+{
+System.out.print("hello");
+}
+}*/
+
+/*
+class Assg12
+{
+	public static void main(String args[])
+	{
+		int a=10;
+		int b=20;
+		if(a>b)
+		{
+			System.out.print("Hello");
+		}
+		else
+		{
+			System.out.print("hi");
+		}
+	}
+}*/
+
+/*
+class Assg12
+{
+	public static void main(String args[])
+	{
+		for(int i=1;i<=5;i++)                       
+		                                                                                
+		{
+			for(int j=1;j<=5;j++)                     
+			{
+				System.out.println("*");
+			}
+			System.out.println();
+		}
+	}
+}*/
+
+/*
+class Assg12
+{
+	public static void main(String args[])
+	{
+		int i=1;
+		while(i<=10)
+		{
+			System.out.print(i);
+			i++;
+		}
+	}
+}*/
+
+/*
+class Assg12{
+	public static void main(String args[])
+	{
+		int i=1;
+		do
+		{
+			System.out.print(i);
+			i++;
+		}while(i<=10);
+	}
+}*/
+/*
+class Assg1
+{
+	public static void main(String args[])
+	{
+		int b[]=new int[20];
+		int index=0;
+		int n=5;
+		
+		while(n>0)
+		{
+			b[index]=n%2;
+			n=n/2;
+			index=index+1;
+		}
+		for(int i=index-1;i>=0;i--)
+		{
+			System.out.print(b[i]);
+		}
+	}
+}
+	
+*/
+/*
+class Assg1
+{
+	public static void main(String args[])
+	{
+		int b[]=new int[20];
+		int index=0;
+		int n=15;
+		
+		while(n>0)
+		{
+			b[index]=n%8;
+			n=n/8;
+			index=index+1;
+		}
+		for(int i=index-1;i>=0;i--)
+		{
+			System.out.print(b[i]);
+		}
+	}
+}	
+*/
+/*
+public class Assg1
+{  
+public static void main(String args[])
+{  
+  
+System.out.println(Integer.toOctalString(8));  
+
+}
+} 
+*/ 
+/*
+public class Assg1
+{  
+public static void main(String args[])
+{  
+  
+System.out.println(Integer.toBinaryString(8));  
+
+}
+}
+
